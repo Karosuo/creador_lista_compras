ba50d217f64d9a7ac335eaeb92f9dacff1ea69bb
Rafael Karosuo <rafaelkarosuo@gmail.com>
2022-11-25 21:50:48 -0700

    Refactor, crear get_src_abspath() para obtener la ruta absoluta de los archivos yaml. Guardar los objetos parseas de los archivos yaml en las variables respectivas en __main__.py. Eliminar el contenedor principal de los archivos yaml, incluir la lista directamente. Modificar recetas.yaml para tener una lista de ingredientes con 'nombre' y 'cantidad' en vez de tener una lista con 'ingrediente' y 'cantidad'.

--- __main__.py
+++ __main__.py
@@ -1,13 +1,19 @@
 # El caso de uso principal
-# - Sumas todas las recetas y lo guardas en "suma_recetas"
-# - A "productos_base" le sumas "suma_recetas" y le restas "refri"
+# Parsea productos_base.yaml y lo guarda como productos_base
+# Parsea refri.yaml y lo guarda como articulos_refri
+# Parsea recetas.yaml y lo guarda como lista_recetas
+# Suma las cantidades de recetas y las convierte en una lista de diccionarios (y las gaurda en suma_recetas)
+# Obtiene la lista de compras como
+# lista_compras = productos_base + suma_recetas - articulos_refri
 
-from utils import yaml_to_python, SRC_BASE_PATH
-from os.path import abspath, join
+from utils import yaml_to_python, get_src_abspath
+
+productos_base = yaml_to_python(get_src_abspath("productos_base.yaml"))
+articulos_refri = yaml_to_python(get_src_abspath("refri.yaml"))
+lista_recetas = yaml_to_python(get_src_abspath("recetas.yaml"))
 
 print("\nGenerador de lista de compras\n")
 
-print("Contenido productos_base: {!s}".format(
-    yaml_to_python(abspath(join(SRC_BASE_PATH, "productos_base.yaml")))
-    
+print("Contenido productos_base: {!s}, tipo recetas: {!s}".format(
+    productos_base, type(productos_base)
 ))
--- utils.py
+++ utils.py
@@ -1,8 +1,34 @@
 import yaml
 from os import path
 import pathlib
+from os.path import abspath, join
 
 SRC_BASE_PATH = pathlib.Path(__file__).parent.resolve()
+
+def suma_ingredientes_recetas(lista_recetas):
+    """
+    Suma los ingredientes de todas las recetas y los pone en una lista de diccionarios con el mismo
+    formato que tienen refri.yaml y productos_base.yaml
+    
+    Parameters
+    ----------
+    lista_recetas : list
+        Lista de diccionarios con el formato
+        - nombre: ""
+          ingredientes:
+          - nombre: ""
+            cantidad: n
+          cantidad: n
+        Que serán mezclados, todos los ingredientes con el mismo nombre, se sumarán sus cantidades y se multiplicarán por la cantidad
+        indicada de recetas
+            
+    Returns
+    -------
+    lista de diccionarios : list
+        Lista con el mismo formato que refri.yaml y productos_base.yaml con todos los ingredientes y sus cantidades
+        sumadas dependiendo de su aparición en las recetas    
+    """
+    pass
 
 def yaml_to_python(yaml_fp):
     """
@@ -17,13 +43,32 @@ def yaml_to_python(yaml_fp):
     pyyaml object
         Dictionary if the yaml element is a hash
         List if the yaml element is a list
-        Empty string if nothing is in the input file
+        NoneType if nothing is in the input file
     Exception
         (FileNotFoundError) If file doesn't exist
         (ScannerError) If the file provided is not parsable, for ex. binary or not yaml
     """
-    yaml_dump = {}
+    parsed_yaml = {}
     with open(yaml_fp, 'r') as yaml_file:
-        yaml_dump = yaml.dump(yaml.safe_load(yaml_file))
+        parsed_yaml = yaml.safe_load(yaml_file)
 
-    return yaml_dump
+    return parsed_yaml
+
+def get_src_abspath(yaml_name):
+    """
+    Obtiene la abspath de cualquiera de los archivos yaml en este directorio
+    
+    Parameters
+    ----------
+    yaml_name : string
+        El nombre del archivo del cual se necesita el abspath
+            
+    Returns
+    -------
+    an absolute path : string
+        La ruta absoluta (abspath) del archivo yaml dado por yaml_name
+    
+    Nota:
+        No se revisa si la ruta es correcta o no
+    """
+    return abspath(join(SRC_BASE_PATH, yaml_name))
