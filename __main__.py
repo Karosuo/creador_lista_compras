910f4c4d35c2a0973fb08924cf4d32de650118d8
Rafael Karosuo <rafaelkarosuo@gmail.com>
2022-11-26 06:13:53 -0700

    Refactor/Add, completar el uso de las funcioens correctas para el caso de uso en __main__.py

--- __main__.py
+++ __main__.py
@@ -11,19 +11,21 @@ from utils import (
     get_src_abspath,
     suma_ingredientes_recetas,
     mezcla_listas,
+    suma_listas,
+    resta_listas,
     print_lista_de_compras
 )
+
 
 productos_base = yaml_to_python(get_src_abspath("productos_base.yaml"))
 articulos_refri = yaml_to_python(get_src_abspath("refri.yaml"))
 lista_recetas = yaml_to_python(get_src_abspath("recetas.yaml"))
 suma_recetas = suma_ingredientes_recetas(lista_recetas)
-# lista_articulos_completos = mezcla_listas(productos_base, suma_recetas)
+lista_articulos_completos = suma_listas(productos_base, suma_recetas)
+lista_de_compras = resta_listas(lista_articulos_completos, articulos_refri)
 
-# print("\n\t...Generador de lista de compras...\n")
-# print("Suma las articulos de las recetas.yaml con los de productos_base.yaml y le resta lo que hay en el refri.yaml\n")
-# print("Lista de compras:\n{!s}".format(
-#     print_lista_de_compras(mezcla_listas(resta=True, lista_articulos_completos, articulos_refri))
-# ))
-
-print("\nLista de recetas:\n{!s}".format(suma_recetas))
+print("\n\t...Generador de lista de compras...\n")
+print("Suma las articulos de las recetas.yaml con los de productos_base.yaml y le resta lo que hay en el refri.yaml\n")
+print("Lista de compras:\n{!s}".format(
+    print_lista_de_compras(lista_de_compras)
+))
