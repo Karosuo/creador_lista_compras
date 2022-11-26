1d257f764dcfc7816918a4c886256bc09fd1f929
Rafael Karosuo <rafaelkarosuo@gmail.com>
2022-11-25 21:19:31 -0700

    Refactor, cambiar comentarios a español en yaml_to_python()

--- utils.py
+++ utils.py
@@ -6,25 +6,24 @@ SRC_BASE_PATH = pathlib.Path(__file__).parent.resolve()
 
 def yaml_to_python(yaml_fp):
     """
-    Reads a yaml file and parses it using 'pyyaml' package
+    Lee un archivo yaml y lo parsea usando el paquete 'pyyaml'    
     Parameters
     ----------
     yaml_fp : string
-        The filepath to the yaml file to be parsed
-    
+        El filepath del archivo yaml a ser parseado
+            
     Returns
     -------
     pyyaml object
         Dictionary if the yaml element is a hash
         List if the yaml element is a list
-        Empty object if nothing in the input file
+        Empty string if nothing is in the input file
     Exception
-        () If file doesn't exist
-        () If the file provided is not parsable, for ex. binary or not yaml
-        () Empty file?
+        (FileNotFoundError) If file doesn't exist
+        (ScannerError) If the file provided is not parsable, for ex. binary or not yaml
     """
     yaml_dump = {}
     with open(yaml_fp, 'r') as yaml_file:
-        yaml_dump = yaml.dump(yaml.safe_load(yaml_file))        
+        yaml_dump = yaml.dump(yaml.safe_load(yaml_file))
 
     return yaml_dump
